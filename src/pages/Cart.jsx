3bf3244253641c913e8bd49f556336468733aa4c
Luis Bisbal <bisbaldaniel@gmail.com>
2023-04-20 11:55:23 -0300

    new styles

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,22 +1,29 @@
 import React from "react";
 import { useContext } from 'react';
 import { CartContext } from '../context';
+import "./pages.css"
 
 
 export const Cart = () => {
   const { itemCount } = useContext(CartContext);
   const { products } = itemCount;
+  let total = 0;
+
+  for (const product of products) {
+    total += product.precio * product.qty;
+  }
 
   return (
-    <div>
+    <div className="cart-container"> 
       <h2>Carrito de Compras</h2>
       <ul>
         {products.map((product) => (
-          <li key={product.id}>
+          <li key={product.id} className="cart-item"> 
             {product.nombre} - {product.qty} - ${product.precio * product.qty}
           </li>
         ))}
       </ul>
+      <p className="cart-total">Total a pagar: ${total}</p> 
     </div>
   );
 };
